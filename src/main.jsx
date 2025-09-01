b0b5ca1df017634d5b978037ae624fcdfa339627
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-09-01 23:04:48 +0530

    - Added new Header, Footer, Home, and About components - Updated App.jsx to use new components

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './components/home/Home.jsx';
+import About from './components/about/About.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children : [
+      {
+        path : "",
+        element : <Home/> 
+      },{
+           path : "/about",
+           element : <About/>
+      }
+    ]
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
